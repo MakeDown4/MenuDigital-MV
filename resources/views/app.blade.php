04d9538c3393933b26ced2741e49520decd25f0b
Vinicius Pires <viniciusapires@yahoo.com.br>
2023-04-15 18:32:30 -0300

    Ajustes layout tela de login/bootstrap fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, inertia-scale=1.0"/>
         @vite('resources/js/app.js')
         @inertiaHead
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-        <script src="{{ mix('/js/app.js') }}" defer></script>
     </head>
     <body>
         @inertia
